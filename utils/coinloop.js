5ce6ed36672fecdc436ebe4debb77052658538cb
Ted Chen <tedchennz@gmail.com>
2018-01-11 02:17:22 +1300

    Parse signals better

--- utils/coinloop.js
+++ utils/coinloop.js
@@ -20,12 +20,34 @@ function waitUntil(asyncTest) {
     });
 }
 
+function rowToSignal($, row) {
+    const header = $(row).find('.ant-row-flex').find('span').find('span');
+    const coin = header.first().text();
+    const bodyRows = $(row).find('.ant-row').find('.ant-row');
+
+    let indicator = bodyRows.first().text();
+    indicator = indicator.substring('Indicator: '.length, indicator.length);
+
+    let signal = bodyRows.last().text();
+    signal = signal.substring('Signal: '.length, signal.length);
+
+    signal = {
+        direction: signal.indexOf('Likelihood of price increase') > -1 ? 'increase' : 'decrease',
+        likelihood: signal.substring(signal.length - 4, signal.length).trim(),
+    }
+    return {
+        coin: coin,
+        indicator: indicator,
+        signal: signal,
+    }
+}
+
 function getAISignals(content) {
     const $ = cheerio.load(content);
     const rows = $('#ai-signals .ant-card .ant-card-bordered');
     const signals = [];
     rows.each((i,row) => {
-        signals.push(($(row).text()));
+        signals.push(rowToSignal($, row));
     })
     return signals;
 }
